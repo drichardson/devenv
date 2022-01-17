d3faf9a286f8aa3f8088f0126b1ccc11976d700c
Doug Richardson <doug@whatnot.com>
2022-01-17 14:48:46 -0800

    more outputs

--- devenvs/doug/main.tf
+++ devenvs/doug/main.tf
@@ -30,14 +30,24 @@ provider "aws" {
 module "ami_picker" {
   source       = "../../modules/ami_picker"
   architecture = "x86_64"
+  distro       = "debian"
 }
 
 module "devenv" {
   source         = "../../modules/devenv"
-  ami            = module.ami_picker.ubuntu
+  ami            = module.ami_picker.debian
   ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJcrrrk512HbXc04iyUdvzM9xAmPnWFWip7MG8sw6NuP"
 }
 
 output "address" {
   value = module.devenv.address
+}
+
+output "user" {
+  value = module.ami_picker.username
+}
+
+output "ssh" {
+  description = "SSH connection string"
+  value       = format("%s@%s", module.ami_picker.username, module.devenv.address)
 }
